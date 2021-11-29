168f35f2d70af67aae0b915591d5b350c45ae770
babreu-ncsa <babreu@illinois.edu>
2021-11-29 12:02:26 -0500

    write everything to h5 file

--- plain/mvmul_plain.f90
+++ plain/mvmul_plain.f90
@@ -6,8 +6,8 @@ program mvmul_plain
         integer, parameter :: i32 = INT32 ! 32-bit integers
 
         ! matrix MxN, vector NxO=1 (easily extendable to matmul)
-        integer(i32), parameter :: M=100_i32
-        integer(i32), parameter :: N=100_i32
+        integer(i32), parameter :: M=10_i32
+        integer(i32), parameter :: N=10_i32
         integer(i32), parameter :: O=1_i32
 
         ! matrix and vectors
@@ -23,15 +23,12 @@ program mvmul_plain
 
         ! HDF5 parameters
         integer(hid_t) :: file_id       ! File identifier
-        integer(hid_t) :: dset_id       ! Dataset identifier
+        integer(hid_t), dimension(3) :: dset_id    ! Dataset identifier
         integer(hid_t) :: dspace_id     ! Dataspace idenfitier
-        integer(hid_t) :: mspace_id     ! Memory dataspace identifier
-        integer(hid_t) :: crp_list      ! Dataset creation property identifier
         integer(i32) :: error           ! Error checks
         integer(i32) :: space_rank      ! Number of dimensions in the data space
-        integer(hsize_t) :: data_dims(1) ! array containing the length of each dimension
+        integer(hsize_t), dimension(2) :: data_dims ! array containing the length of each dimension
         character(len=20) :: filename
-        filename = "mvmul.h5"
 
 
         ! ALLOCATE ARRAYS
@@ -47,30 +44,6 @@ program mvmul_plain
         call random_number(vec)
 
 
-        ! open HDF5 interface
-        call h5open_f(error)
-        
-        ! open the HDF5 file
-        call h5fcreate_f(filename, H5F_ACC_TRUNC_F, file_id, error)
-
-        space_rank = 1
-        data_dims(1) = N
-
-        ! open data space
-        call h5screate_simple_f(space_rank, data_dims, dspace_id, error)
-
-        ! create dataset
-        call h5dcreate_f(file_id, "vec", H5T_NATIVE_DOUBLE, dspace_id, dset_id, error)
-
-        ! write data set
-        call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, vec(1:N,1), data_dims, error)
-
-        call h5dclose_f(dset_id,error)   
-        call h5sclose_f(dspace_id, error)
-        call h5fclose_f(file_id, error)
-        call h5close_f(error)
-
-
         ! MAT-VEC MULTIPLICATION
         call cpu_time(startT)
         do k = 1, N
@@ -84,9 +57,54 @@ program mvmul_plain
 
         write(*,*) 'MAT-VEC multiplication took: ', (endT-startT), ' s'
 
+
+        filename = "mvmul.h5"
+        ! open HDF5 interface
+        call h5open_f(error)
+        ! open the HDF5 file
+        call h5fcreate_f(filename, H5F_ACC_TRUNC_F, file_id, error)
+
+        ! write mat
+        space_rank = 2
+        data_dims(1) = M
+        data_dims(2) = N
+        ! open data space
+        call h5screate_simple_f(space_rank, data_dims, dspace_id, error)
+        ! create dataset
+        call h5dcreate_f(file_id, "mat", H5T_NATIVE_DOUBLE, dspace_id, dset_id(1), error)
+        ! write data set
+        call h5dwrite_f(dset_id(1), H5T_NATIVE_DOUBLE, mat, data_dims, error)
+        call h5dclose_f(dset_id(1),error)   
+        call h5sclose_f(dspace_id, error)
+        
+        ! write vec
+        space_rank = 2
+        data_dims(1) = N
+        data_dims(2) = O
+        call h5screate_simple_f(space_rank, data_dims, dspace_id, error)
+        call h5dcreate_f(file_id, "vec", H5T_NATIVE_DOUBLE, dspace_id, dset_id(2), error)
+        call h5dwrite_f(dset_id(2), H5T_NATIVE_DOUBLE, vec, data_dims, error)
+        call h5dclose_f(dset_id(2), error)
+        call h5sclose_f(dspace_id, error)
+
+        ! write prod
+        space_rank = 2
+        data_dims(1) = M
+        data_dims(2) = O
+        call h5screate_simple_f(space_rank, data_dims, dspace_id, error)
+        call h5dcreate_f(file_id, "prod", H5T_NATIVE_DOUBLE, dspace_id, dset_id(2), error)
+        call h5dwrite_f(dset_id(2), H5T_NATIVE_DOUBLE, prod, data_dims, error)
+        call h5dclose_f(dset_id(2), error)
+        call h5sclose_f(dspace_id, error)
+
+        ! close file and interface
+        call h5fclose_f(file_id, error)
+        call h5close_f(error)
+
+
+        ! clean up
         deallocate(mat)
         deallocate(vec)
         deallocate(prod)
-
 
 end program mvmul_plain
