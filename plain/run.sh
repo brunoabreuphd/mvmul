024d4e2f1a24480c042cd9e5bb86407bdf9cbacf
Bruno Abreu <babreu@illinois.edu>
2022-04-20 10:24:30 -0500

    update run for delta

--- plain/run.sh
+++ plain/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/jet/home/babreu/Libraries/hdf5/gcc10.2/lib
-./mvmul_plain
+module purge
+module load nvhpc/22.2
+module load openmpi/4.1.2
+module load hdf5/1.13.1
+
+./mvmul_plain.exe
